0056acc999f01c6e3d9b3a24f644487527f83cf6
Ingmar Drewing <ingmar@drewing.de>
2015-04-24 17:25:05 +0200

    Added Item and Inventory classes

---
+++ Assets/Code/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory  {
+
+	private Item[] items;
+	private GameControllerScript gmc;
+
+	public Inventory(GameControllerScript gmc_param){
+		gmc = gmc_param;
+	}
+}
---
+++ Assets/Code/items/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract class Item {
+	// 2D Representation
+	// 3D Representation
+	
+	// Obvious Name
+	public string obvious_name;
+
+	// Obvious Description
+	public string obvious_description;
+
+	// Informed Name
+	public string informed_name;
+
+	// Informed Desciption
+	public string informed_description;
+		
+	// the target upon which the item is used, e. g. the user, an enemy, another item
+	public Object target;
+}
